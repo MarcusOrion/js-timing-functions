1eea65f83333375ed233d0fd9ab1e7e933430226
MarcusOrion <manvati.marco@gmail.com>
2026-09-09 11:40:34 +0200

    add first exercise (allo scadere dei 3 secondi)

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Utilizzare le timing functions di JavaScript per risolvere le seguenti traccie:
+
+//     mostra un alert 3 secondi dopo il caricamento della pagina
+const clock = setTimeout(countdown, 3000);
+function countdown() {
+  alert("Attenzione, ci sei riuscito!");
+}
+//     al click su un elemento, cambia il colore dello sfondo dopo un ritardo
+//     rendi visibile un testo nascosto dopo alcuni secondi dal caricamento
+//     simula un reindirizzamento: al click su un tasto cambia testo di un paragrafo e ricarica la pagina dopo X secondi
+//     simula uno stato di "caricamento": cambia il testo di un elemento appena cliccato un pulsante e cambialo nuovamente dopo qualche secondo
+//     mostra l'orario corrente aggiornandolo ogni secondo
+//     fai lampeggiare un titolo alternandone il colore ogni mezzo secondo
+//     anima una progress bar aumentando la sua larghezza gradualmente fino al 100%
+//     mostra un nuovo numero casuale ogni X secondi
+//     cambia automaticamente il colore di sfondo della pagina ad intervalli regolari
